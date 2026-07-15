6df983f6302e48fefa9b356985357829f19cc192
Mostafiz <rbmostafiz211@gmail.com>
2026-07-15 10:05:46 +0600

    mostafiz?

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+students = []
 def main():
     print('''
     1. Add Student
-    2. View Students
-    3. Search Student
-    4. Update Student
-    5. Delete Student
-    6. Exit''')
-    students = []
+    2. Search Student
+    3. Update Student
+    4. Delete Student
+    5. Exit''')
     Search_option = input("Enter your search option : ")
-    if Search_option == '1' or Search_option == "Add Student":
+    if Search_option == '1' or Search_option.lower() == "Add Student":
         # Adding Student
         def Add_Student():
             name = input("Enter student's name: ")
@@ -20,19 +19,38 @@ def main():
                 "student_id": student_id,
                 "department": department
             }
-
+            print(student)
             students.append(student)
+            print(students)
         Add_Student()
         print("Student added successfully.")
-    elif Search_option == '2' or Search_option == "Search Student":
+    elif Search_option == '2' or Search_option.lower() == "Search Student":
         # Searching Option
+        def searching():
             search = input("Enter the name of the student to search: ")
             for student in students:
-                if search == student["name"] or search == student["student_id"]:
+                print(student)
+                if search.lower() == student["name"].lower() or search == student["student_id"].lower():
                     print(student)
-                break
-            else: 
-                print("student not found")
+                    break
+                else: 
+                    print("student not found")
+    elif Search_option == "3" or Search_option.lower() == "Update Student":
+        def update():
+            given_id = input("Enter Student ID to update: ")
+            for student in students:
+                if given_id.lower() == student["student_id"]:
+                    print(student)
+            
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     print('Application started')
